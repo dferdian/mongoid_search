d40a627d06944c0d87f88034838e937859be83c8
dferdian <dhendy_ferdian@yahoo.com>
2011-03-03 12:59:37 +0700

    handle broken undefined mb_chars for Fixnum if text is an Integer

--- lib/mongoid_search/keywords_extractor.rb
+++ lib/mongoid_search/keywords_extractor.rb
@@ -1,6 +1,8 @@
 class KeywordsExtractor
   def self.extract(text)
     return [] if text.blank?
-    text.mb_chars.normalize(:kd).to_s.gsub(/[^[:alpha:]\s\.\-_:;'",]/,'').downcase.split(/[\s\.\-_:;'",]+/)
+    ## handle broken undefined mb_chars for Fixnum if text is an Integer
+    ## convert text to string before calling mb_chars method
+    text.to_s.mb_chars.normalize(:kd).to_s.gsub(/[^[:alpha:]\s\.\-_:;'",]/,'').downcase.split(/[\s\.\-_:;'",]+/)
   end
 end
